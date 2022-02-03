dc39a3453ce8fb9ad744f9b2254c0d7e99871896
Krushn Dayshmookh <krushndayshmookh@gmail.com>
2022-02-03 09:42:16 +0530

    follower management

--- controllers/user.js
+++ controllers/user.js
@@ -73,3 +73,120 @@ exports.delete_delete = async (req, res) => {
   //   return res.status(500).send({ err })
   // }
 }
+
+
+exports.followers_get = async (req, res) => {
+  let { userId } = req.params
+
+  let query = {
+    user: userId,
+  }
+
+  let populate = [
+    {
+      path: 'follower',
+      select: 'username avatar',
+    },
+  ]
+
+  let sort = {
+    'follower.username': 1,
+  }
+
+  try {
+    let result = await Follower.find(query).populate(populate).sort(sort).lean()
+
+    let followers = result.map(f => f.follower)
+
+    return res.send(followers)
+  } catch (err) {
+    console.error({ err })
+    return res.status(500).send({ err })
+  }
+}
+
+exports.isFollowing_get = async (req, res) => {
+  let { user } = req.decoded
+  let { userId } = req.params
+
+  if (user._id == userId) {
+    return res.send({ isFollowing: true })
+  }
+
+  try {
+    let result = await Follower.findOne({
+      user: userId,
+      follower: user._id,
+    })
+
+    let isFollowing = !!result
+
+    return res.send({ isFollowing })
+  } catch (err) {
+    console.error({ err })
+    return res.status(500).send({ err })
+  }
+}
+
+exports.follower_put = async (req, res) => {
+  let { user } = req.decoded
+  let { userId } = req.params
+
+  try {
+    const userToFollow = await User.findById(userId)
+
+    if (!userToFollow) {
+      return res.status(404).send({
+        error: 'User not found',
+      })
+    }
+
+    const follower = await Follower.findOne({
+      user: userId,
+      follower: user._id,
+    })
+
+    if (follower) {
+      return res.status(400).send({
+        error: 'You have already followed this user',
+      })
+    }
+
+    let newFollower = new Follower({
+      user: userId,
+      follower: user._id,
+    })
+
+    await newFollower.save()
+
+    return res.send({ success: true })
+  } catch (err) {
+    console.error({ err })
+    return res.status(500).send({ err })
+  }
+}
+
+exports.follower_delete = async (req, res) => {
+  let { user } = req.decoded
+  let { userId } = req.params
+
+  try {
+    let follower = await Follower.findOne({
+      user: userId,
+      follower: user._id,
+    })
+
+    if (!follower) {
+      return res.status(404).send({
+        error: 'Follower not found',
+      })
+    }
+
+    await follower.remove()
+
+    return res.send({ success: true })
+  } catch (err) {
+    console.error({ err })
+    return res.status(500).send({ err })
+  }
+}
--- routes/user.js
+++ routes/user.js
@@ -6,13 +6,29 @@ const userController = require('../controllers/user')
 
 // Controllers -----
 
-router.get('/:userid', validateToken, userController.details_get)
+router.get('/:userId', validateToken, userController.details_get)
+
+router.get('/:userId/followers', userController.followers_get)
+
+router.get(
+  '/:userId/followers/isFollowing',
+  validateToken,
+  userController.isFollowing_get
+)
+
+router.put('/:userId/followers', validateToken, userController.follower_put)
+
+router.delete(
+  '/:userId/followers',
+  validateToken,
+  userController.follower_delete
+)
 
 router.get('/', validateToken, userController.list_get)
 
 router.post('/', validateToken, userController.create_post)
 
-router.delete('/:userid', validateToken, userController.delete_delete)
+router.delete('/:userId', validateToken, userController.delete_delete)
 
 router.get('/', (req, res) => {
   res.send('Please read documentation for the API. (user)')
