76359847730e83b14e1967771d7be657faae4460
Krushn Dayshmookh <krushndayshmookh@gmail.com>
2022-02-03 15:19:12 +0530

    cleanup

--- supabase.js
+++
@@ -1,8 +0,0 @@
-const createClient = require('@supabase/supabase-js').createClient
-
-const supabaseUrl = process.env.SUPABASE_URL
-const supabaseServiceKey = process.env.SUPABASE_SERVICE_KEY
-
-const supabase = createClient(supabaseUrl, supabaseServiceKey)
-
-module.exports = supabase
