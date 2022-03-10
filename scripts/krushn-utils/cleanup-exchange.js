207059f52b46a1a2c5e8981cb84a5b0ef23a4be4
Krushn Dayshmookh <krushndayshmookh@gmail.com>
2022-03-10 06:01:19 +0530

    exchange reset scipt

---
+++ scripts/krushn-utils/cleanup-exchange.js
@@ -0,0 +1,41 @@
+const performDatabaseUpdate = require('../performDatabaseUpdate')
+
+const {
+  BlockDelta,
+  Holding,
+  Instrument,
+  Order,
+  Payment,
+  Trade,
+  User,
+} = require('../../models')
+
+async function run() {
+  try {
+    // cleanup BlockDeltas, Orders, Trades, Payments and Holdings
+    await BlockDelta.deleteMany({})
+    await Order.deleteMany({})
+    await Trade.deleteMany({})
+    await Payment.deleteMany({})
+    await Holding.deleteMany({})
+
+    // reset Intruments
+    await Instrument.updateMany(
+      {},
+      { ltp: 10, fresh: 0, minted: 0, floating: 0, delta: 0 }
+    )
+
+    // reset User.chips
+    await User.updateMany({}, { chips: 0 })
+
+    console.log('done')
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+const main = async () => {
+  await performDatabaseUpdate(run)
+}
+
+main()
