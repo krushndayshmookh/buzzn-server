dc4c51d9f4528a7a009de0dc30fe5b83cdfe5c90
Krushn Dayshmookh <krushndayshmookh@gmail.com>
2022-07-15 11:29:47 +0530

    ouch

--- scripts/deploy/application_start.sh
+++ scripts/deploy/application_start.sh
@@ -4,6 +4,7 @@
 
 # Stop all servers and start the server as a daemon
 # forever stopall
-# forever start /home/ec2-user/nodejs/app.js
+# forever start/home/ec2-user/nodejs/server.js
+cd /home/ec2-user/nodejs
 PORT=80
 npm start
