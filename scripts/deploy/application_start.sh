05253168b77c8705e38e823837330d23eba54220
Krushn Dayshmookh <krushndayshmookh@gmail.com>
2022-07-15 12:33:10 +0530

    fix

--- scripts/deploy/application_start.sh
+++ scripts/deploy/application_start.sh
@@ -5,6 +5,7 @@ cp /home/ec2-user/default_env /home/ec2-user/nodejs/.env
 . ~/.nvm/nvm.sh
 
 # Stop all servers and start the server as a daemon
+cd /home/ec2-user/nodejs
 pm2 stop all
-pm2 start /home/ec2-user/nodejs/server.js
+pm2 start server.js
 
